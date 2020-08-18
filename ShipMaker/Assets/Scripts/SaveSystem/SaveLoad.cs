91c3923c76a6f02d3c67e8aef4764c8f67c339c8
Axel BARBIER <axel.barbier@epita.fr>
2020-08-18 18:33:11 +0200

    manage fileaccess exception and we are done

--- ShipMaker/Assets/Scripts/SaveSystem/SaveLoad.cs
+++ ShipMaker/Assets/Scripts/SaveSystem/SaveLoad.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using System.Security.Cryptography;
 
 
 public class SaveLoad
@@ -14,17 +15,24 @@ public class SaveLoad
 
         formatter.Serialize(stream, content);
         stream.Close();
+
+        EncryptFile(path, path + "i");
     }
 
 
     public string LoadAs(string name)
     {
         string path = Application.persistentDataPath + "/" + name + ".chancla";
-        string s = "";
+
+        DecryptFile(path, path);
+
         BinaryFormatter formatter = new BinaryFormatter();
         FileStream stream = new FileStream(path, FileMode.Open);
-        s = formatter.Deserialize(stream) as string;
+        string s = formatter.Deserialize(stream) as string;
         stream.Close();
+
+        EncryptFile(path, path);
+
         return s;
     }
 
@@ -32,11 +40,75 @@ public class SaveLoad
     public string LoadAsset(string name)
     {
         string path = Application.streamingAssetsPath + "/" + name + ".chancla";
-        string s = "";
+
+        DecryptFile(path, path);
+
         BinaryFormatter formatter = new BinaryFormatter();
         FileStream stream = new FileStream(path, FileMode.Open);
-        s = formatter.Deserialize(stream) as string;
+        string s = formatter.Deserialize(stream) as string;
         stream.Close();
+
+        EncryptFile(path, path);
+
         return s;
+    }
+
+
+    private void EncryptFile(string inputFile, string outputFile)
+    {
+        //try
+        //{
+            byte[] key = new byte[] { 103, 9, 84, 234, 104, 93, 29, 2, 192, 140, 28, 182, 249, 189, 48, 59, 84, 234, 104, 93, 29, 2, 192, 140 };
+            byte[] IV = new byte[] { 103, 9, 84, 234, 104, 93, 29, 2, 192, 140, 28, 93, 29, 2, 192, 140 };
+
+            string cryptFile = outputFile;
+            FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+            RijndaelManaged RMCrypto = new RijndaelManaged();
+
+            CryptoStream cs = new CryptoStream(fsCrypt,
+                RMCrypto.CreateEncryptor(key, IV),
+                CryptoStreamMode.Write);
+            
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            int data;
+            while ((data = fsIn.ReadByte()) != -1)
+                cs.WriteByte((byte)data);
+
+
+            fsIn.Close();
+            cs.Close();
+            fsCrypt.Close();
+        //}
+        //catch
+        //{
+        //    throw new ArgumentNullException("error");
+        //}
+    }
+
+
+    private void DecryptFile(string inputFile, string outputFile)
+    {
+        byte[] key = new byte[] { 103, 9, 84, 234, 104, 93, 29, 2, 192, 140, 28, 182, 249, 189, 48, 59, 84, 234, 104, 93, 29, 2, 192, 140 };
+        byte[] IV = new byte[] { 103, 9, 84, 234, 104, 93, 29, 2, 192, 140, 28, 93, 29, 2, 192, 140 };
+
+        FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+
+        RijndaelManaged RMCrypto = new RijndaelManaged();
+
+        CryptoStream cs = new CryptoStream(fsCrypt,
+            RMCrypto.CreateDecryptor(key, IV),
+            CryptoStreamMode.Read);
+
+        FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+        int data;
+        while ((data = cs.ReadByte()) != -1)
+            fsOut.WriteByte((byte)data);
+
+        fsOut.Close();
+        cs.Close();
+        fsCrypt.Close();
     }
 }
